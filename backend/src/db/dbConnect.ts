a3b14ea1f5b7195c20c5ab5d911bc717d678fd96
tilak-dev <tsingh99991@gmail.com>
2024-11-18 14:48:06 +0530

    prisma setup

--- backend/src/db/dbConnect.ts
+++ backend/src/db/dbConnect.ts
@@ -1,19 +1,5 @@
-import mongoose from "mongoose";
-import { DATABASE_NAME } from "../constants";
+import { PrismaClient } from "@prisma/client";
 
-// Connect to MongoDB
-const dbConnect = async () => {
-  try {
-    const connectionInstanse = await mongoose.connect(
-      `${process.env.MONGO_URI}/${DATABASE_NAME}`
-    );
-    console.log(
-      `db connected successfully , Host: ${connectionInstanse.connection.host}`
-    );
-  } catch (error) {
-    console.log("error in connect mongodb", error);
-    process.exit(1);
-  }
-};
+const prisma = new PrismaClient();
 
-export default dbConnect;
+export default prisma;
